4d8f40afb68211aa05afb4b3b33142c52d4442c0
18345609582 <18345609582@163.com>
2022-08-03 16:24:52 +0800

    final_statementinfo修改2

--- src/components/finalStatementInfo.vue
+++ src/components/finalStatementInfo.vue
@@ -9,53 +9,66 @@
           </div>
         </div>
 
+      <template v-for="(item, index) in data.final_statement_info" :key="index">
         <div class="layui-form-item" pane>
           <div class="layui-inline" style="width: 100%;margin-bottom:0px;height: 38px;">
             <div class="layui-input-inline" style="margin-left:0px ;">
 <!--              <input type="text" v-model="data.final_statement_info[0].name" placeholder="请输入原被告" autocomplete="off"-->
 <!--                     class="layui-input" style="width: 100%;float: left;">-->
-              <VueMultiselect :option-height="38" :show-labels="false" v-model="data.final_statement_info[0].name"
+              <VueMultiselect :option-height="38" :show-labels="false" v-model="data.final_statement_info[index].name"
                               :options="getPlaintiffName" placeholder="请选择原被告"
                               style="line-height: 16px;width: 210px; min-height: 38px"></VueMultiselect>
             </div>
             <div class="layui-input-block">
               <div class="myselect-div">
+<!--                <VueMultiselect :option-height="38" :show-labels="false"-->
+<!--                                v-model="data.final_statement_info[index].final_statement"-->
+<!--                                :options=option_statement placeholder="请选择陈述意见"-->
+<!--                                :taggable="true"-->
+<!--                                style="line-height: 16px; min-height: 38px;float:left;width: 90%;"></VueMultiselect>-->
                 <input type="text" v-model="data.final_statement_info[0].final_statement"   placeholder="请输入最后陈述意见" autocomplete="off"
                        class="layui-input" style="width: 90%;float: left;">
                 <button @click="add_component('final_statement_info')" type="button" class="layui-btn layui-btn-sm"
                         data-type="text" style="float: right;">
                   <i class="layui-icon">&#xe654;</i>
                 </button>
+                <div v-show="index!='0'">
+                  <button @click="delete_component('final_statement_info',index)" type="button" class="layui-btn layui-btn-sm layui-btn-danger"
+                          data-type="text" style="float: right;margin-right:5px">
+                    <i class="layui-icon">&#xe640;</i>
+                  </button>
+                </div>
               </div>
 
             </div>
           </div>
         </div>
+      </template>
 
-        <template v-for="(item, index) in data.final_statement_info.slice(1)" :key="index">
-          <div class="layui-form-item" pane>
-            <div class="layui-inline" style="width: 100%;margin-bottom:0px;height: 38px;">
-              <div class="layui-input-inline" style="margin-left:0px ;">
-                <VueMultiselect :option-height="38" :show-labels="false" v-model="data.final_statement_info[index+1].name"
-                                :options="getPlaintiffName" placeholder="请选择原被告"
-                                style="line-height: 16px;width: 210px; min-height: 38px"></VueMultiselect>
-<!--                <input type="text" v-model="data.final_statement_info[index+1].name" placeholder="请输入原被告" autocomplete="off"-->
-<!--                     class="layui-input" style="width: 100%;float: left;">-->
-              </div>
+<!--        <template v-for="(item, index) in data.final_statement_info.slice(1)" :key="index">-->
+<!--          <div class="layui-form-item" pane>-->
+<!--            <div class="layui-inline" style="width: 100%;margin-bottom:0px;height: 38px;">-->
+<!--              <div class="layui-input-inline" style="margin-left:0px ;">-->
+<!--                <VueMultiselect :option-height="38" :show-labels="false" v-model="data.final_statement_info[index+1].name"-->
+<!--                                :options="getPlaintiffName" placeholder="请选择原被告"-->
+<!--                                style="line-height: 16px;width: 210px; min-height: 38px"></VueMultiselect>-->
+<!--&lt;!&ndash;                <input type="text" v-model="data.final_statement_info[index+1].name" placeholder="请输入原被告" autocomplete="off"&ndash;&gt;-->
+<!--&lt;!&ndash;                     class="layui-input" style="width: 100%;float: left;">&ndash;&gt;-->
+<!--              </div>-->
 
-            <div class="layui-input-block">
-              <div class="myselect-div">
-                <input type="text" v-model="data.final_statement_info[index+1].final_statement"  placeholder="请输入最后陈述意见" autocomplete="off"
-                       class="layui-input" style="width: 90%;float: left;">
-                <button @click="delete_component('final_statement_info',index+1)" type="button" class="layui-btn layui-btn-sm layui-btn-danger"
-                        data-type="text" style="float: right;">
-                  <i class="layui-icon">&#xe640;</i>
-                </button>
-              </div>
-            </div>
-          </div>
-          </div>
-        </template>
+<!--            <div class="layui-input-block">-->
+<!--              <div class="myselect-div">-->
+<!--                <input type="text" v-model="data.final_statement_info[index+1].final_statement"  placeholder="请输入最后陈述意见" autocomplete="off"-->
+<!--                       class="layui-input" style="width: 90%;float: left;">-->
+<!--                <button @click="delete_component('final_statement_info',index+1)" type="button" class="layui-btn layui-btn-sm layui-btn-danger"-->
+<!--                        data-type="text" style="float: right;">-->
+<!--                  <i class="layui-icon">&#xe640;</i>-->
+<!--                </button>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--          </div>-->
+<!--        </template>-->
       </form>
     </div>
   </div>
@@ -82,7 +95,8 @@ export default {
       data = wholeItem.finalStatementInfo
     }
     return {
-      name_list: [],
+      option_statement:['坚持诉讼请求','坚持答辩意见'],
+      // name_list: [],
       data: data,
     };
 
